c42677376488d7d8614c69a5268634e04f22c313
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-11-08 16:11:52 +0900

    add interceptor to axios

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const axios = require('axios');
 
+axios.interceptors.response.use((res) => {
+  console.log(res)
+  return res;
+}, (error) => {
+  console.log(error)
+  return Promise.reject(error);
+});
 
 axios.get('http://localhost:7000/error')
   .then(res => {
     console.log(res.data);
   })
   .catch(err => {
-    console.error(err.response);
+    console.error(err.response.data);
   });
